52c8efa073de0fabc7cf7e22c1cb12fac0a146ff
Your Name <you@example.com>
2023-05-24 03:49:22 +0800

    Added Toastify alertMessage

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const axios = require ('axios');
+const dotenv= require('dotenv').config();
 
 const isDev = true;
 
@@ -9,6 +10,8 @@ const createWindow = () => {
     width: isDev ? 1200 : 600,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
       preload: path.join(__dirname, "preload.js"),
     },
   });
@@ -39,31 +42,33 @@ app.on("window-all-closed", () => {
   }
 });
 
-async function openAI(event,sentence) { 
-  let res = null;
+async function openAI(event, topic) { 
+  let result = null;
+
+  const env = dotenv.parsed;
 
   await axios({
       method: 'post',
       url: 'https://api.openai.com/v1/completions',
       data: {
-    model: "text-davinci-003",
-    prompt: "Correct this to standard English:\n\n " + sentence,
-    temperature: 0,
-    max_tokens: 60,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0
-      },
+          model: "text-davinci-003",
+          prompt: "Create a list of 8 questions for my interview about"+ topic,
+          temperature: 0.5,
+          max_tokens: 150,
+          top_p: 1.0,
+          frequency_penalty: 0.0,
+          presence_penalty: 0.0
+        },
       headers: {'Content-Type': 'application/json',
-      'Authorization': 'Bearer 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 '
+      'Authorization': 'Bearer '+ env.APIKEY_OPENAI
     }
   })
     .then (function(response){
-      res= response.data;
+      result= response.data;
     })
     .catch(function(error){
-      res = error;
+      result = error;
     });
     
-    return res;
+    return result;
 }
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,11 +1,33 @@
-const form = document.getElementById("form_sentence");
+const form = document.getElementById("interview_form");
 if (form) {
   form.onsubmit = async function (e) {
     e.preventDefault();
 
     const formData = new FormData(form);
-      console.log(formData.get("sentence"));
-      const response= await window.axios.openAI(formData.get("sentence"));
-      document.getElementById("sentence_corrected").innerHTML=JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
-  }; 
-} 
+
+    let topic = formData.get("topic");
+
+    if (topic.length <= 2) {
+      alertMessage("error", "Please input at least 8 characters!");
+      return;
+    }
+
+    const response = await window.axios.openAI(formData.get("topic"));
+    document.getElementById("int_ques").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
+  };
+}
+
+function alertMessage(status, topic){
+  window.Toastify.showToast({
+    text: topic,
+    duration: 5000,
+    stopOnFocus: true,
+    style: {
+      textAlign: "center",
+      background: status == "error" ? "red":"green",
+      color: "white",
+      padding: "5px",
+      marginTop: "2px"
+    }
+  });
+}
