c4fc344ef4e799c1f22feced40b279612e12b868
Your Name <you@example.com>
2023-05-25 02:05:49 +0800

    Added menubar function

--- main.js
+++ main.js
@@ -1,34 +1,129 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("path");
 const axios = require ('axios');
 const dotenv= require('dotenv').config();
 
 const isDev = true;
 
+
+const isMac = process.platform === 'darwin';
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'About',
+        click: aboutWindow
+      },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'minimize' },
+      { type: 'separator' },
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  }
+];
+
+// Main Window
 const createWindow = () => {
-  const win = new BrowserWindow({
+
+  const main = new BrowserWindow({
     width: isDev ? 1200 : 600,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: true,
-      preload: path.join(__dirname, "preload.js"),
+          nodeIntegration: true,
+          contextIsolation: true,
+          preload:path.join(__dirname,"preload.js"),
     },
   });
 
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
   if (isDev) {
-    win.webContents.openDevTools();
+    main.webContents.openDevTools();
   }
 
-  win.loadFile(path.join(__dirname, "./renderer/index.html"));
+  main.loadFile(path.join(__dirname, "./renderer/index.html"));
 };
 
-app.whenReady().then(() => {
-  ipcMain.handle('axios.openAI',openAI )
+function aboutWindow () {
+  const about = new BrowserWindow({
+    width: 400,
+    height: 400,
+    alwaysOnTop: true,
+  });
 
- 
+  about.setMenuBarVisibility(false);
+
+  about.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+app.whenReady().then(() => {
+  // Initialize Functions
+  ipcMain.handle('axios.openAI', openAI);
+
+  // Create Main Window
   createWindow();
 
+  // Start Window
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -36,13 +131,15 @@ app.whenReady().then(() => {
   });
 });
 
+// Close Window
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
-});
+  });
 
-async function openAI(event, topic) { 
+
+async function openAI (event, topic) { 
   let result = null;
 
   const env = dotenv.parsed;
@@ -52,7 +149,7 @@ async function openAI(event, topic) {
       url: 'https://api.openai.com/v1/completions',
       data: {
           model: "text-davinci-003",
-          prompt: "Create a list of 8 questions for my interview about"+ topic,
+          prompt: "Create a list of 8 questions for my interview about:\n"+ topic,
           temperature: 0.5,
           max_tokens: 150,
           top_p: 1.0,
@@ -72,3 +169,4 @@ async function openAI(event, topic) {
     
     return result;
 }
+  
